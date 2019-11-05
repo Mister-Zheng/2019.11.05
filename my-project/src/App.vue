5b9afeba33c4ff168c5db294977792717d0d1e3b
zyx <1030007267@qq.com>
2019-11-05 14:51:52 +0800

    测试提交20191105

---
+++ my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <one/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import One from './components/one'
+export default {
+  name: 'App',
+  components:{
+    One,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
---
+++ my-project/src/components/one.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="one">
+        <h1>{{msg}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            msg:'我是新来的!!!'
+        }
+    }
+}
+</script>
+<style scoped>
+h1{
+    font-size:24px;
+}
+</style>
